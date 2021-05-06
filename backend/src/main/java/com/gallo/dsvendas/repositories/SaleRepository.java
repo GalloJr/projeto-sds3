bc6f84565cfa60636e607c5c4d8d12e1298cb1d1
GalloJr <renato.gallo@fatec.sp.gov.br>
2021-05-05 21:59:14 -0300

    Pagination

---
+++ backend/src/main/java/com/gallo/dsvendas/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.gallo.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gallo.dsvendas.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
