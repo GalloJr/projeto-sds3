841dad4692a2087a0cff9b50b7b00c77f153b113
GalloJr <renato.gallo@fatec.sp.gov.br>
2021-05-05 21:43:11 -0300

    Layers

---
+++ backend/src/main/java/com/gallo/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.gallo.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gallo.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
---
+++ backend/src/main/java/com/gallo/dsvendas/service/SellerService.java
@@ -0,0 +1,23 @@
+package com.gallo.dsvendas.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gallo.dsvendas.dto.SellerDTO;
+import com.gallo.dsvendas.entities.Seller;
+import com.gallo.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll() {
+		List<Seller> result = repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+	}
+}
